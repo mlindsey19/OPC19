99982eaf72166fd046d5db12a49788a968ccfcef
Mitch Lindsey <dmld54@mail.umsl.edu>
2019-05-17 16:06:43 -0500

    asdf

--- prog8.py
+++ prog8.py
@@ -1,75 +1,39 @@
 import sys
 x = sys.argv[1]
+c = len(sys.argv[1:])
 # Python program to for tree traversals 
 
   # A class that represents an individual node in a 
   # Binary Tree 
 class Node: 
-	def __init__(self,key): 
-	self.left = None
-	self.right = None
-	self.val = key 
-        
-  def printInorder(root): 	    
-  	if root: 
-	          # First recur on left child 
-          printInorder(root.left) 
-				    
-           # then print the data of node 
-          print(root.val), 
-						      
-             # now recur on right child 
-          printInorder(root.right) 
-								        
-  # A function to do postorder tree traversal 
-  def printPostorder(root): 
-										      
-       if root: 
-											    
-      # First recur on left child 
-         printPostorder(root.left) 
-														      
-        # the recur on right child 
-         printPostorder(root.right) 
-																        
-     # now print the data of node 
-   print(root.val), 
-		    
-#A function to do preorder tree traversal 
-  def printPreorder(root): 
-																      
-     if root: 
-																	    
-        # First print the data of node 
-       print(root.val), 
-																	      
-      
-       printPreorder(root.left) 
+    def __init__(self,key): 
+       self.left = None
+       self.right = None
+       self.val = key
 
-																			       printPreorder(root.right) 
-root = Node(1) 
-root.left      = Node(2) 
-root.right     = Node(3) 
-root.left.left  = Node(4) 
-root.left.right  = Node(5) 
-print "Preorder traversal of binary tree is"
-printPreorder(root) 
-																				      
-print "\nInorder traversal of binary tree is"
-printInorder(root) 															
-print "\nPostorder traversal of binary tree is"
-printPostorder(root)
 
-for c in x:
-    if c in vowel:
-        i = vowel.index(c)
-        out += vowel[(i+1)%vv]
-    elif c in cons:
-        i = cons.index(c)
-        out += cons[(i+1)%cc]
+def insert(root, node):
+    if root is None:
+        root =node
     else:
-        e=1
-if (e):
-    print 'ERROR'
-else:
-    print out
+        if root.val < node.val:
+            if root.right is None:
+                root.right =node
+            else:
+                insert(root.right, node)
+        else:
+            if root.left is None:
+                root.left =node
+            else:
+                 insert(root.left, node)
+
+def printPreorder(root):
+     if root: 
+       print(root.val), 
+       printPreorder(root.left) 
+       printPreorder(root.right) 
+root= Node(int(sys.argv[1]))
+for i in range(c-1):
+    insert(root,Node(int(sys.argv[i+2])))
+
+printPreorder(root) 
